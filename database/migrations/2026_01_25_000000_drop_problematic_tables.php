7fb6d229614ded3f65718cbb9156911035af2482
santijank <ticallcenter@gmail.com>
2026-01-24 00:24:27 +0700

    Add migration to drop problematic tables

---
+++ database/migrations/2026_01_25_000000_drop_problematic_tables.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::dropIfExists('registration_participants');
+        Schema::dropIfExists('registrations');
+    }
+
+    public function down()
+    {
+        // No rollback needed
+    }
+};
